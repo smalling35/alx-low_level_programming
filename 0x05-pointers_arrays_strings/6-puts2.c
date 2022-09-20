f2b546f5da2a73aa9465deacb20edb13b230f28b
smalling35 <yakubujbshaibu92@gmail.com>
2022-09-19 21:41:42 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,14 +5,14 @@
 * puts2 - prints every other character
 * @str: string
 *
-*Return: nothing
+* Return: nothing
 */
 
-void puts2(char *str)
+void puts(char *str)
 {
 	int i = 0;
-	
-	while(*(str + i) != '\0')
+
+	while (*(str + i) != '\0')
 	{
 		if (i % 2 == 0)
 			putchar(*(str + i));
