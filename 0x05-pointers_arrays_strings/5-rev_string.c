5b3ca988347a6119479f09458156cf7cfc6d8082
smalling35 <yakubujbshaibu92@gmail.com>
2022-09-20 13:36:49 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,14 +5,14 @@
  *  @s: input string
  *  Return: string in reverse
  */
-void rev_string (char *s)
+void rev_string(char *s)
 
 {
 	char rev = s[0];
 	int counter = 0;
 	int i;
 
-	while(s[counter] != '\0')
+	while (s[counter] != '\0')
 		counter++;
 	for (i = 0; i < counter; i++)
 	{
