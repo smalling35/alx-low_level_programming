6e935d42b9fb7b8f2d8f070e31f0a2c35b55ae0a
smalling35 <yakubujbshaibu92@gmail.com>
2022-09-29 17:08:38 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,36 +13,20 @@ char *_strstr(char *haystack, char *needle)
 
 	while (haystack[i])
 	{
-
 		while (needle[j] && (haystack[i] == needle[0]))
-
 		{
-
 			if (haystack[i + j] == needle[j])
-
 				j++;
-
 			else
-
 				break;
-
 		}
-
 		if (needle[j])
-
 		{
-
 			i++;
-
 			j = 0;
-
 		}
-
 		else
-
 			return (haystack + i);
-
 	}
-
 	return (0);
-
+}
