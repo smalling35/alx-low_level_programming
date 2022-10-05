7efd6feedf74f880caaa53eefd0b06f0ebf66925
smalling35 <yakubujbshaibu92@gmail.com>
2022-10-05 07:56:58 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* create_array - creates an array of chars, and
+* initializes it with a specific char.
+* @size: size of the array
+* @c: character to inserted
+* Return: NULL if the size is zero or fails
+* pointer to array if everthing is normal.
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int index;
+
+	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
+	for (index = 0; index < size; index++)
+		array[index] = c;
+	return (array);
+}
